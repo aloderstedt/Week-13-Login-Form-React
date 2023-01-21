36b6be626c2c24d1df67b9e531dc921f0b9ae7af
Anthony <114252009+aloderstedt@users.noreply.github.com>
2023-01-20 17:30:36 -0800

    fixed dependencies I hope

--- src/Components/loginbutton.js
+++ src/Components/loginbutton.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class LoginButton extends React.Component {
     render() {
         return (
-            <button>Log In</button>
+            <button id="logButt">Log In</button>
         ); 
     }
 }
